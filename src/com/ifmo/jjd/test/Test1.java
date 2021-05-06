e634fd104b6f3fb4cadbbe0dcd977d84d50584a9
dana <daneliya.karelina@gmail.com>
2021-05-06 16:53:22 +0300

    TEST-6 test update

---
+++ src/com/ifmo/jjd/test/Test1.java
@@ -0,0 +1,8 @@
+package com.ifmo.jjd.test;
+
+public class Test1 {
+    public static void main(String[] args) {
+        String testString = "hello" ;
+        System.out.println(testString);
+    }
+}
